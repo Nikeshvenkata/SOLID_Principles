b0d5304734ee8b7c028fc8d2bab147d90a3b912b
Nikesh Venkata <47297267+Nikeshvenkata@users.noreply.github.com>
2024-09-12 21:24:44 +0530

    Add files via upload

---
+++ design_principles - SRP - hands on done/src/main/java/com/company/singleresp/Store.java
@@ -0,0 +1,23 @@
+package com.company.singleresp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//Stores data in memory
+public class Store {
+
+    private static final Map<String, User> STORAGE = new HashMap<>();
+
+    public void store(User user) {
+        synchronized(STORAGE) {
+            STORAGE.put(user.getName(), user);
+            //System.out.println(STORAGE);
+        }
+    }
+
+    public User getUser(String name) {
+        synchronized(STORAGE) {
+            return STORAGE.get(name);
+        }
+    }
+}
